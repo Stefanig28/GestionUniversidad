15ae71cfbee24d6c4faa8846f8e71c5099c76431
Stefani Torres <152030588+Stefanig28@users.noreply.github.com>
2024-07-16 07:38:10 -0500

    Update Curso.java

--- src/main/java/com/example/GestionUniversidad/Model/Curso.java
+++ src/main/java/com/example/GestionUniversidad/Model/Curso.java
@@ -13,7 +13,7 @@ public class Curso {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "curso_id")
-    private Long id;
+    private Long id
 
     @Column(name = "nombre")
     private String nombre;
