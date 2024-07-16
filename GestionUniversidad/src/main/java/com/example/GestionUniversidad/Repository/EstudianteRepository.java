9315640bf930a4b70eb058906296548a1ef43401
Stefani Torres <152030588+Stefanig28@users.noreply.github.com>
2024-07-16 07:26:59 -0500

    Delete GestionUniversidad directory

--- GestionUniversidad/src/main/java/com/example/GestionUniversidad/Repository/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.GestionUniversidad.Repository;
-
-import com.example.GestionUniversidad.Model.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
-}
